d83c8b01595efe4d44e9382f9e4785a6a7a42785
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-07-29 20:14:06 +0300

    Create Login Page and Processes

---
+++ RabbitMQ.ExcelApp.UI.Web/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQ.ExcelApp.UI.Web.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
