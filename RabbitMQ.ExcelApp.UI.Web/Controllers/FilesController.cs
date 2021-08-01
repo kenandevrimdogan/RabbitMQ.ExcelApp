9e78b7ad9e8003f4399a4a657ac9fbae4eb9fb5d
kenandevrimdogan <kenandogandevrim@gmail.com>
2021-08-01 14:29:07 +0300

    Add Files Api Controller for Upload Action

---
+++ RabbitMQ.ExcelApp.UI.Web/Controllers/FilesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.ExcelApp.UI.Web.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.ExcelApp.UI.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+            var userFile = await _appDbContext.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create);
+
+            await file.CopyToAsync(stream);
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+
+            await _appDbContext.SaveChangesAsync();
+            //signalR notfication oluşturulacak
+
+            return Ok();
+        }
+    }
+}
